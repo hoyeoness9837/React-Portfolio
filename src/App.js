1f8f7af16aeb5a1cfc5269abcbadad24e131c4b7
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-06-25 20:16:12 -0700

    updated with html and stylings.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Link, Route, BrowserRouter as Router } from 'react-router-dom';
+import AboutMe from './pages/AboutMe';
+import Project from './pages/Project';
+import Main from './pages/Main';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav className='navbar navbar-expand-lg navbar-dark'
+        style={{ backgroundColor: 'red' }}>
+          <Link to='/'>Main</Link>
+          <Link to='/aboutme'>About Me</Link>
+          <Link to='/projects'>Projects</Link>
+        </nav>
+        <Switch>
+          <Route exact path='/'>
+            <Main />
+          </Route>
+          <Route path='/aboutme'>
+            <AboutMe />
+          </Route>
+          <Route path='/projects'>
+            <Project />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
