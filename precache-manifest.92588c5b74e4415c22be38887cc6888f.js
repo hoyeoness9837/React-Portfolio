eaf1f7395bc892afe7153b15e84fa8d8f3eb02f4
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-06-25 20:15:17 -0700

    Updates

---
+++ precache-manifest.92588c5b74e4415c22be38887cc6888f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e377e1dc66f9d3328cdf86bb7ce97b83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edf047a04d1c9570266b",
+    "url": "/static/css/main.defc96c1.chunk.css"
+  },
+  {
+    "revision": "b97cf0c5e7ad214b8db0",
+    "url": "/static/js/2.fc5b72a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fc5b72a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edf047a04d1c9570266b",
+    "url": "/static/js/main.e9ba658c.chunk.js"
+  },
+  {
+    "revision": "57e57f65dd6914a13205",
+    "url": "/static/js/runtime-main.7316b2c8.js"
+  }
+]);
